941db50e787c1ecee32fec906d574afc3e1a92f6
Hasan Raza <hasanrazakhan89@outlook.com>
2023-12-06 13:45:52 +0530

    all files added

---
+++ revise_inputNode.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const { url } = require("inspector");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+//root level listening for form input
+app.get("/", (req, res) => {
+  res.send(`
+  <form action="/email" method="POST">
+  <label>email</label>
+  <input type="email" name="eMail" id="e-mail">
+  <button>Slap Email !!</button>
+  </form>
+    `);
+});
+
+//posting the input of the form
+app.post("/email", (req, res) => {
+  const emailInput = req.body.eMail;
+  const filePath = path.join(__dirname, "email", "emails.json");
+  const fileData = fs.readFileSync(filePath);
+  const existingEmails = JSON.parse(fileData);
+  existingEmails.push(emailInput);
+  fs.writeFileSync(filePath, JSON.stringify(existingEmails));
+  console.log(`${emailInput} - has just registered!!`);
+  res.send("You'll be notified whenever wo go online !!");
+});
+
+//only for niggas!!
+app.get("/nigga", (req, res) => {
+  res.send("BITCHY ASS NIGGA");
+});
+
+//port ot listen for the navigation
+app.listen(1920, () => {
+  console.log("This is a revision test-api running on localhost:1920");
+});
